37a607bbabab97a9f6e89daadfe8fabc75a26b31
Mansij Ranjit <mansijranjit@gmail.com>
2022-12-22 19:04:04 +0545

    Day1

---
+++ Day1.py
@@ -0,0 +1,46 @@
+# print("Supermansij")
+
+# a="5"
+# b="4"
+# c=a+b
+# print(c)
+
+""" Else IF """
+# letter="A"
+# if letter == "B":
+#     print("Letter is B")
+# elif letter == "C" or letter=="A":
+#     print("Letter is C")
+# elif letter == "A":
+#     print("Letter is A")
+# else:
+#   print("Letter isnt A,B or C")
+  
+""" For Loop"""
+# n=4
+# for i in range(0,n):
+#  print(i)
+
+# for i in range(1,5):
+#   for j in range(i):
+#     print(i,end=' ')
+#   print()
+
+"""Exception Handling"""  
+# try:
+#   print(x)
+# except:
+#     print("An exception occoured")
+
+"""User input"""
+# name=input("What is your name?")
+# print(name)
+
+# num1=int(input("Enter First number:"))
+# num2=int(input("Enter second number:"))
+# print(num1+num2)
+
+"""Write a program to input first and last name and print last name and then firstname"""
+firstname=input("Enter First name:")
+lastname=input("Enter last name:")
+print("Hello"+ " "+ lastname+" "+firstname)
